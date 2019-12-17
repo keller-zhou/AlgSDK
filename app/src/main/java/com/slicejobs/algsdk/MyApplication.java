6894e6428edc17b09118fde4e22ea5dbba08b89c
joel <joel.yuan@slicejobs.com.cn>
2019-12-17 17:29:26 +0800

    去掉分包

--- app/src/main/java/com/slicejobs/algsdk/MyApplication.java
+++ app/src/main/java/com/slicejobs/algsdk/MyApplication.java
@@ -8,15 +8,10 @@ import com.slicejobs.algsdk.algtasklibrary.app.SliceApp;
 
 public class MyApplication extends Application {
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(base);
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        SliceApp.getInstance().init(this);
+        //SliceApp.getInstance().init(this);
     }
 }
